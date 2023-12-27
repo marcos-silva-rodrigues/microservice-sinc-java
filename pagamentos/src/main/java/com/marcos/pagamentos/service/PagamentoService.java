65d3dcdd150edbdd4a5d46ad8a6ebba46cf79c8f
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-12-27 19:05:31 -0300

    feat: service

---
+++ pagamentos/src/main/java/com/marcos/pagamentos/service/PagamentoService.java
@@ -0,0 +1,56 @@
+package com.marcos.pagamentos.service;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import com.marcos.pagamentos.dto.PagamentoDto;
+import com.marcos.pagamentos.model.Pagamento;
+import com.marcos.pagamentos.model.Status;
+import com.marcos.pagamentos.repository.PagamentoRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class PagamentoService {
+
+  @Autowired
+  private ModelMapper modelMapper;
+
+  @Autowired
+  private PagamentoRepository repository;
+
+  public Page<PagamentoDto> getAll(Pageable paginacao) {
+    return repository
+        .findAll(paginacao)
+        .map(p -> modelMapper.map(p, PagamentoDto.class));
+  }
+
+  public PagamentoDto getById(Long id) {
+    Pagamento pagamento = repository.findById(id)
+        .orElseThrow(() -> new EntityNotFoundException());
+
+    return modelMapper.map(pagamento, PagamentoDto.class);
+  }
+
+  public PagamentoDto create(PagamentoDto dto) {
+    Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+    pagamento.setStatus(Status.CRIADO);
+    repository.save(pagamento);
+
+    return modelMapper.map(pagamento, PagamentoDto.class);
+  }
+
+  public PagamentoDto update(Long id, PagamentoDto dto) {
+    Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+    pagamento.setId(id);
+    pagamento = repository.save(pagamento);
+    return modelMapper.map(pagamento, PagamentoDto.class);
+  }
+
+  public void remove(Long id) {
+    repository.deleteById(id);
+  }
+}
