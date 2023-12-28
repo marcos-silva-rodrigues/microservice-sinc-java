32e77b1950394c9b3a8139f77620e7ae21b21f71
marcos-silva-rodrigues <marcos.rodrigues.dev@gmail.com>
2023-12-28 20:17:34 -0300

    feat: pedido microservice

---
+++ pedidos/src/main/java/com/marcos/pedidos/dto/ItemDoPedidoDto.java
@@ -0,0 +1,14 @@
+package com.marcos.pedidos.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemDoPedidoDto {
+
+    private Long id;
+    private Integer quantidade;
+    private String descricao;
+}
---
+++ pedidos/src/main/java/com/marcos/pedidos/model/Status.java
@@ -0,0 +1,12 @@
+package com.marcos.pedidos.model;
+
+public enum Status {
+    REALIZADO,
+    CANCELADO,
+    PAGO,
+    NAO_AUTORIZADO,
+    CONFIRMADO,
+    PRONTO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE;
+}
